d3eb49ce1113fa862b4544c1987f6664f593e3e1
ma.kuznetsova <you@example.com>
2024-11-18 13:41:56 +0300

    test4

---
+++ BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int b) {
+        int a = 20;
+        // а - 1 бонусная миля, рублей
+        int result = b/a;
+        return result;
+    }
+}
