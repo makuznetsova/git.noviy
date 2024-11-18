a033eca4f2d5ba2e5ab9fd1ec9375beac275d4c6
ma.kuznetsova <you@example.com>
2024-11-18 13:43:35 +0300

    test5

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+// для первого задачия:
+
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("привет, мир");
+//        System.out.println("2+3");
+//    }
+//}
+
+// Домашнее задание к занятию «Testability. Введение в ООП»
 public class Main {
     public static void main(String[] args) {
-        System.out.println("привет, мир");
-        System.out.println("2+3");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
